94f17e1e8b871cd5ea47e167f28f68bcb4b6531d
qianchao <980569038@qq.com>
2021-05-10 13:07:20 +0800

    追加创建、删除索引功能及相关配置

--- src/main/java/cn/itinfor/java/Index.java
+++
@@ -1,7 +0,0 @@
-package cn.itinfor.java;
-
-public class Index {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/cn/itinfor/java/ProductDao.java
@@ -0,0 +1,8 @@
+package cn.itinfor.java;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends ElasticsearchRepository<Product, Long> {
+}
